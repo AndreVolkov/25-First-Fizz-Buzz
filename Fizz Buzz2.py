d6dbd020eab1275a628de395a81f630ffbbf0713
Ande <a.volkov@lajm.lt>
2023-02-13 22:57:55 +0200

    add function FizzBuzz

--- Fizz Buzz2.py
+++ Fizz Buzz2.py
@@ -1,12 +1,23 @@
 # 25 first "Fizz Buzz"
 
 def FizzBuzz(n):
-    i = 1
-    k = 0
-    while k != n:
-        if i % 3 == 0 and i % 5 == 0:
-            k = k + 1
-            print(k,'Fizz Buzz -',i)
-        i = i + 1
-FizzBuzz(25)
+    if n % 3 == 0 and n % 5 == 0:
+        print('Fizz Buzz', end='=')
+    elif n % 3 == 0:
+        print('Fizz')
+    elif n % 5 == 0:
+        print('Buzz')
+    else:
+        print(n)
+    return FizzBuzz
+
+i = 1
+k = 0
+while k != 25:
+    if i % 3 == 0 and i % 5 == 0:
+        k = k + 1
+        print(k, end='. ')
+        FizzBuzz(i)
+        print(i)
+    i = i + 1
 
